c24e95bbed15260766e5a44243b8cfdfffeb155e
Suwijak Promsatid <suwijak.pak@gmail.com>
2025-07-08 11:50:13 +0700

    cf deploy

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -2,3 +2,5 @@
 export async function GET() {
     return Response.json({ status: 'ok', environment: process.env.NEXTJS_ENV });
 }
+
+export const runtime = 'edge'; // Use edge runtime for better performance
